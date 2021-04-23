a51791eddede96e5c94e434a0e34d4661af64c14
Rohit Bhati <rohitbhati8833@gmail.com>
2021-04-23 15:25:08 +0530

    Users functionality was added.

--- index.js
+++ index.js
@@ -6,31 +6,29 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 
 app.use(express.static(__dirname + '/public'));
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
-
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index');
 });
+const users = {};
 
 io.on('connection', (socket) => {
     // When a user connects
-    console.log('A User Connected');
-    // When a message is sent
-    socket.on('chat message', msg => {
-        // console.log(`Message: ${msg}`);
+    socket.on('new-user-joined', newUserName => {
+        users[socket.id] = newUserName;
+        console.log(`${newUserName} joined the chat`);
+        io.emit('new-user-joined', newUserName);
     });
-    // Sending the message to others
+    // Sending and Recieving the message
     socket.on('chat message', msg => {
-        io.emit('chat message', msg);
+        socket.broadcast.emit('chat message', {msg:msg, name:users[socket.id]});
     });
     // When a user disconnects
     socket.on('disconnect', () => {
         console.log('User Disconnected');
+        socket.broadcast.emit('user-disconnected',users[socket.id]);
     });
 });
 
--- public/js/app.js
+++ public/js/app.js
@@ -3,17 +3,41 @@ let messages = document.getElementById('messages');
 let form = document.getElementById('form');
 let input = document.getElementById('input');
 
+let userName = prompt("Enter your name to join Global Chat");
+if (userName) {
+    socket.emit('new-user-joined', userName);
+}
+
 form.addEventListener('submit', e => {
     e.preventDefault();
     if (input.value) {
+        let item = document.createElement('li');
+        item.textContent = input.value;
+        item.classList.add('right');
+        messages.appendChild(item);
         socket.emit('chat message', input.value);
         input.value = "";
     }
 });
 
-socket.on('chat message', msg => {
+socket.on('new-user-joined', userName => {
     let item = document.createElement('li');
-    item.textContent = msg;
+    item.textContent = `${userName} joined the chat`;
+    item.classList.add('center');
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+})
+socket.on('chat message', data => {
+    let item = document.createElement('li');
+    item.textContent = `${data.name} : ${data.msg}`;
+    item.classList.add('left');
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
+socket.on('user-disconnected', userName => {
+    let item = document.createElement('li');
+    item.textContent = `${userName} left the chat`;
+    item.classList.add('center');
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+})
